53e1e02fe8f4583cd2190c9661f06534cad09d17
Timothy Curchod <timofeyc@hotmail.com>
2015-10-11 12:10:35 +1100

    Updated the readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # curator
+
+A project to manage artists and thier paintings.
+
+The GitHub repo is called curator, but the npm repo is named art-curator since curator already exists as an npm modile.
--- src/index.js
+++ src/index.js
@@ -4,3 +4,7 @@ module.exports = {
   getArtists: artists,
   getArtist: uniqueRandowmArray(artists)
 };
+// repl commands to test
+// var lib = require('./src/index.js');
+// lib.getArtists
+// lib.getArtist()
