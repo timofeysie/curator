b8cb0cce85e8128ef9127db1563f6fd0e0789825
Timothy Curchod <timofeyc@hotmail.com>
2015-10-11 14:43:04 +1100

    Added search function

--- src/index.js
+++ src/index.js
@@ -2,9 +2,25 @@ var artists = require('./data/artists.json');
 var uniqueRandowmArray = require('unique-random-array');
 module.exports = {
   getArtists: artists,
-  getArtist: uniqueRandowmArray(artists)
+  getArtist: uniqueRandowmArray(artists),
+  searchArtists: function(term) {
+  	console.log('term',term);
+  	for (var i = 0; i < artists.length; i++) {
+        var obj = artists[i];
+        for(var key in obj) {
+            var attrValue = obj[key];
+            //console.log(key+' '+attrValue);
+            if (attrValue.toLowerCase().indexOf(term.toLowerCase()) !== -1) {
+            	console.log(key+' '+attrValue);
+            	return attrValue;
+            }
+        }
+    }
+    return null;
+  }
 };
 // repl commands to test
 // var lib = require('./src/index.js');
 // lib.getArtists
 // lib.getArtist()
+// lib.searchArtists('emily');
