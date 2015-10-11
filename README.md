9013e2859a0a4803bda22315a63bbbd16be4acc8
Timothy Curchod <timofeyc@hotmail.com>
2015-10-11 15:11:33 +1100

    Added getBio function

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 A project to manage artists and thier paintings.
 
-The GitHub repo is called curator, but the npm repo is named art-curator since curator already exists as an npm modile.
+The GitHub repo is called curator, but the npm repo is named art-curator since curator already exists as an npm module.
--- src/index.js
+++ src/index.js
@@ -1,26 +1,43 @@
 var artists = require('./data/artists.json');
 var uniqueRandowmArray = require('unique-random-array');
 module.exports = {
-  getArtists: artists,
-  getArtist: uniqueRandowmArray(artists),
-  searchArtists: function(term) {
-  	console.log('term',term);
-  	for (var i = 0; i < artists.length; i++) {
-        var obj = artists[i];
-        for(var key in obj) {
-            var attrValue = obj[key];
-            //console.log(key+' '+attrValue);
-            if (attrValue.toLowerCase().indexOf(term.toLowerCase()) !== -1) {
-            	console.log(key+' '+attrValue);
-            	return attrValue;
-            }
-        }
-    }
+  	getArtists: artists,
+  	getArtist: uniqueRandowmArray(artists),
+  	searchArtists: function(term) {
+	  	console.log('term',term);
+	  	for (var i = 0; i < artists.length; i++) {
+	        var obj = artists[i];
+	        for(var key in obj) {
+	            var attrValue = obj[key];
+	            //console.log(key+' '+attrValue);
+	            if (attrValue.toLowerCase().indexOf(term.toLowerCase()) !== -1) {
+	            	console.log(key+' '+attrValue);
+	            	return obj;
+	            }
+	        }
+	    }
+    	return null;
+  	},
+  	getBio: function(term) {
+  		console.log('term',term);
+  		var obj = this.searchArtists(term);
+  		if (obj) {
+  			var fileName = obj.Artist.replace(/ /g,"_");
+  			try {
+				var artist = require('./data/'+fileName+'.json');
+				return artist;
+			} catch (error)
+			{
+				console.log('probelm: '+error);
+			}
+  		}
     return null;
-  }
+  	}
 };
-// repl commands to test
+// repl commands to test:
 // var lib = require('./src/index.js');
 // lib.getArtists
 // lib.getArtist()
-// lib.searchArtists('emily');
+// lib.searchArtists('abie');
+// lib.getBio('albert');
+
