69ee2a7f48e180456577faeb568182d112087251
Timothy Curchod <timofeyc@hotmail.com>
2018-07-04 10:53:45 +1000

    Added tests for he createWikiDataUrl function

--- src/index.test.js
+++ src/index.test.js
@@ -5,6 +5,91 @@ import curator from'./index.js';
 // var curator = require('./index.js');
 
 describe('curator', function() {
+
+	/* --- createWikiDataUrl --- */
+	describe('createWikiDataUrl', function () {
+		const dataUrl = curator.createWikiDataUrl();
+		it('should return a string', function() {
+			expect(dataUrl).to.be.a('string');
+		});
+		console.log('lll', dataUrl);
+		it('should contain a sparql string', function() {
+			expect(dataUrl).to.equal('https://query.wikidata.org/sparql?format=json&query=%0A%20%20%20%20%20%20%20%20SELECT%20%3Fcognitive_bias%20%3Fcognitive_biasLabel%20%3Fcognitive_biasDescription%20WHERE%20%7B%0A%20%20%20%20%20%20%20%20%20%20%20%20SERVICE%20wikibase%3Alabel%20%7B%20%0A%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20bd%3AserviceParam%20wikibase%3Alanguage%20%22%5BAUTO_LANGUAGE%5D%2Cen%22.%20%0A%20%20%20%20%20%20%20%20%20%20%20%20%7D%0A%20%20%20%20%20%20%20%20%20%20%20%20%3Fcognitive_bias%20wdt%3AP31%20wd%3AQ1127759.%0A%20%20%20%20%20%20%20%20%7D%0A%09%09LIMIT%201000');
+		});
+		it('should contain the base url for a sparql API query', function() {
+			expect(dataUrl).to.contain('https://query.wikidata.org/sparql');
+		});
+	});
+
+	// createWikiMediaUrl: createWikiMediaUrl,
+	// describe('getArtists', function () {
+	// 	it('should return a list of artists', function() {
+	// 		expect(curator.getArtists).to.satisfy(isArrayOfObjects);
+
+	// 		function isArrayOfObjects(array) {
+	// 			return array.every(function(item) {
+	// 				return typeof item === 'object';
+	// 			});
+	// 		}
+	// 	});
+	// 	it('should contain someone', function () {
+	// 		var randomArtist = curator.getArtist();
+	// 		expect(curator.getArtists).to.include(randomArtist);
+	// 	});
+	// });
+
+	// parseWikiMediaResult: parseWikiMediaResult,
+	// describe('getArtists', function () {
+	// 	it('should return a list of artists', function() {
+	// 		expect(curator.getArtists).to.satisfy(isArrayOfObjects);
+
+	// 		function isArrayOfObjects(array) {
+	// 			return array.every(function(item) {
+	// 				return typeof item === 'object';
+	// 			});
+	// 		}
+	// 	});
+	// 	it('should contain someone', function () {
+	// 		var randomArtist = curator.getArtist();
+	// 		expect(curator.getArtists).to.include(randomArtist);
+	// 	});
+	// });
+
+	// parseSingeWikiMediaPage: parseSingeWikiMediaPage,
+	// describe('getArtists', function () {
+	// 	it('should return a list of artists', function() {
+	// 		expect(curator.getArtists).to.satisfy(isArrayOfObjects);
+
+	// 		function isArrayOfObjects(array) {
+	// 			return array.every(function(item) {
+	// 				return typeof item === 'object';
+	// 			});
+	// 		}
+	// 	});
+	// 	it('should contain someone', function () {
+	// 		var randomArtist = curator.getArtist();
+	// 		expect(curator.getArtists).to.include(randomArtist);
+	// 	});
+	// });
+
+	// createSingleWikiMediaPageUrl: createSingleWikiMediaPageUrl,
+	// describe('getArtists', function () {
+	// 	it('should return a list of artists', function() {
+	// 		expect(curator.getArtists).to.satisfy(isArrayOfObjects);
+
+	// 		function isArrayOfObjects(array) {
+	// 			return array.every(function(item) {
+	// 				return typeof item === 'object';
+	// 			});
+	// 		}
+	// 	});
+	// 	it('should contain someone', function () {
+	// 		var randomArtist = curator.getArtist();
+	// 		expect(curator.getArtists).to.include(randomArtist);
+	// 	});
+	// });
+
+	/* ----- Original Tests --------- */
 	describe('getArtists', function () {
 		it('should return a list of artists', function() {
 			expect(curator.getArtists).to.satisfy(isArrayOfObjects);
